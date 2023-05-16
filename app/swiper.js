25812984e8b8f1bbe9a95d248eb0e2004d7dfb95
Christian Oliveira <christianoliver2010@gmail.com>
2023-05-16 20:09:50 -0300

    Update project slider functionality and dynamic loading
    Updated the JavaScript code to fetch project data from a JSON API and dynamically load the slides in the Swiper carousel. Also, refactored the code to ensure proper initialization of the Swiper after adding the slides. This allows for seamless looping of the slides and improved flexibility for adding more projects in the future.

--- app/swiper.js
+++ app/swiper.js
@@ -1,42 +1,73 @@
-const swiper = new Swiper('.swiper', {
-
-    loop: true,
-    speed: 400,
-    centeredSlides: true,
-    allowTouchMove: true,
-    pagination: {
+function initializeSwiper() {
+    const swiper = new Swiper('.swiper', {
+      loop: true,
+      speed: 400,
+      centeredSlides: true,
+      allowTouchMove: true,
+      pagination: {
         el: '.swiper-pagination',
         type: 'bullets',
-    },
-    // autoplay: {
-    //     delay: 2000
-    // },
-    // effect: 'cards',
-    cardsEffect: {
+      },
+      autoplay: {
+        delay: 2000
+      },
+      cardsEffect: {
         slideShadows: false
-    },
-
-    navigation: {
+      },
+      navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
-    },
-    breakpoints: {
-        // when window width is >= 
+      },
+      breakpoints: {
         320: {
-            slidesPerView: 1,
-            spaceBetween: 2
+          slidesPerView: 1,
+          spaceBetween: 2
         },
-        // when window width is >= 
         750: {
-            slidesPerView: 2,
-            spaceBetween: 15
+          slidesPerView: 2,
+          spaceBetween: 15
         },
-        // when window width is >= 
         1024: {
-            slidesPerView: 2.5,
-            spaceBetween: 15    
+          slidesPerView: 2.5,
+          spaceBetween: 15    
         }
-    
-    }
-});
-
+      }
+    });
+  }
+  
+  let projects = [];
+  const endPointAPI = 'app/projects.json';
+  fetchProjects();
+  
+  async function fetchProjects() {
+    const res = await fetch(endPointAPI);
+    projects = await res.json();
+  }
+  
+  const swiperWrapper = document.querySelector('.swiper-wrapper');
+  
+  function showProjectsOnPage(projectsList) {
+    swiperWrapper.innerHTML = "";
+    projectsList.forEach(project => {
+      swiperWrapper.innerHTML +=
+        `<div class="swiper-slide">
+           <a target="_blank" href="${project.link}">
+             <div class="img__and__see-more">
+               <img class="swiper__img" src="${project.image}" alt="${project.title}">
+               <a target="_blank" href="${project.link}" class="swiper-slide__see-more">See more</a>
+             </div>
+           </a>
+           <div class="swiper__info__container">
+             <h3 class="swiper-slide__title">${project.title}</h3>
+             <ul class="swiper-slide__tags">
+               ${project.tags.map(tag => `<li class="swiper-slide__tag"><i class="fab fa-html5"></i> ${tag}</li>`).join('')}
+             </ul>
+           </div>
+         </div>`;
+    });
+  }
+  
+  fetchProjects().then(() => {
+    showProjectsOnPage(projects);
+    initializeSwiper();
+  });
