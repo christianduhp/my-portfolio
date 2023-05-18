3926018413974abe8430215ead7b846c35d58b05
Christian Oliveira <christianoliver2010@gmail.com>
2023-05-18 12:57:48 -0300

    Fixed bug in mobiles

--- app/dynamic-text.js
+++ app/dynamic-text.js
@@ -5,6 +5,7 @@ function handleElementMouseOver(elementSelector, textElementSelector, titleEleme
   const defaultText = textElement.innerHTML;
   const defaultTitle = titleElement.innerHTML;
 
+  fetch('app/data.json')
   fetch('app/app-json/about.json')
     .then(response => response.json())
     .then(data => {
@@ -15,34 +16,14 @@ function handleElementMouseOver(elementSelector, textElementSelector, titleEleme
           textElement.innerHTML = text;
           titleElement.innerHTML = title;
         });
-
-        element.addEventListener('mouseleave', () => {
-          textElement.innerHTML = defaultText;
-          titleElement.innerHTML = defaultTitle;
-        });
-
-        element.addEventListener('touchstart', () => {
-          const { title, text } = dataArray[index];
-          textElement.innerHTML = text;
-          titleElement.innerHTML = title;
-        });
-
-        element.addEventListener('touchend', () => {
+    
+        element.addEventListener('mouseout', () => {
           textElement.innerHTML = defaultText;
           titleElement.innerHTML = defaultTitle;
         });
       });
     })
     .catch(error => console.error(error));
-
-  document.addEventListener('touchstart', (event) => {
-    const isClickedElementDescendant = elements.some(element => element.contains(event.target));
-    if (!isClickedElementDescendant) {
-      textElement.innerHTML = defaultText;
-      titleElement.innerHTML = defaultTitle;
-    }
-  });
 }
-
 handleElementMouseOver('.interests-box', '.about__paragraph', '.about__title', 'interests');
 handleElementMouseOver('.skill__container', '.skill__paragraph', '.skills__title', 'skills');
