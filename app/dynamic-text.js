e11637fe0d17f6479c3f183d4e6d6c187f7f3ef5
Christian Oliveira <christianoliver2010@gmail.com>
2023-05-18 12:18:59 -0300

    Add mobile touch support for element interaction
    Update the code to add mobile touch support for element interaction. Previously, the code only supported mouse events for displaying JSON text and restoring default text. With this update, the code now includes touch event listeners to provide a similar functionality for mobile devices. The touchstart event displays the JSON text when the user touches the corresponding element, and the touchend event restores the default text when the touch is released. Additionally, a touch event listener on the document ensures that the default text is restored when the user touches outside the elements. This enhancement improves the user experience by making the element interaction consistent across both desktop and mobile platforms.

--- app/dynamic-text.js
+++ app/dynamic-text.js
@@ -4,6 +4,7 @@ function handleElementMouseOver(elementSelector, textElementSelector, titleEleme
   const titleElement = document.querySelector(titleElementSelector);
   const defaultText = textElement.innerHTML;
   const defaultTitle = titleElement.innerHTML;
+  let isElementActive = false;
 
   fetch('app/app-json/about.json')
     .then(response => response.json())
@@ -12,17 +13,45 @@ function handleElementMouseOver(elementSelector, textElementSelector, titleEleme
       elements.forEach((element, index) => {
         element.addEventListener('mouseover', () => {
           const { title, text } = dataArray[index];
+          if (!isElementActive) {
+            textElement.innerHTML = text;
+            titleElement.innerHTML = title;
+          }
+        });
+
+        element.addEventListener('mouseout', () => {
+          if (!isElementActive) {
+            textElement.innerHTML = defaultText;
+            titleElement.innerHTML = defaultTitle;
+          }
+        });
+
+        element.addEventListener('touchstart', () => {
+          const { title, text } = dataArray[index];
           textElement.innerHTML = text;
           titleElement.innerHTML = title;
+          isElementActive = true;
         });
-    
-        element.addEventListener('mouseout', () => {
-          textElement.innerHTML = defaultText;
-          titleElement.innerHTML = defaultTitle;
+
+        element.addEventListener('touchend', () => {
+          isElementActive = false;
+          if (!isElementActive) {
+            textElement.innerHTML = defaultText;
+            titleElement.innerHTML = defaultTitle;
+          }
         });
       });
     })
     .catch(error => console.error(error));
+
+  document.addEventListener('touchstart', (event) => {
+    const isClickedElementDescendant = elements.some(element => element.contains(event.target));
+    if (!isClickedElementDescendant) {
+      isElementActive = false;
+      textElement.innerHTML = defaultText;
+      titleElement.innerHTML = defaultTitle;
+    }
+  });
 }
 
 handleElementMouseOver('.interests-box', '.about__paragraph', '.about__title', 'interests');
